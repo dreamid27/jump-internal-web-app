2e31d533e7cbd905d2483a8b68055e3db058f980
Siswanto <siswantosmik@gmail.com>
2021-05-19 17:40:59 +0700

    add react routes

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,5 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Button = () => <button />;
+const Button = () => {
+    return (
+        <div>
+            <a>
+                <Link to="/">Home</Link>
+            </a>
+            <a>
+                <Link to="/mentee">Mentee</Link>
+            </a>
+            <a>
+                <Link to="/mentors">Mentors</Link
+                ></a>
+        </div>
+    )
+}
 
 export default Button;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,1 +1,30 @@
 //TODO: implement react router here and make routing for all of pages (home, mentors, mentee)
+import React from "react";
+import Home from '../pages/Home';
+import Mentee from '../pages/Mentee';
+import Mentors from '../pages/Mentors';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from "react-router-dom";
+
+const Routes = () => {
+    return (
+        <div>
+            <Switch>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+                <Route path="/mentee">
+                    <Mentee />
+                </Route>
+                <Route path="/mentors">
+                    <Mentors />
+                </Route>
+            </Switch>
+        </div>
+    )
+}
+
+export default Routes;
